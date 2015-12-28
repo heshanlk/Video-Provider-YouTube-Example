b18073d29ee0354946fd95cefee99929791b59b3
Heshan Wanigasooriya <heshan@heidisoft.com>
2015-12-28 17:15:02 -0500

    Create YouTube.php

---
+++ src/Plugin/video/Provider/YouTube.php
@@ -0,0 +1,55 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\video\Plugin\video\Provider\YouTube.
+ */
+
+namespace Drupal\video\Plugin\video\Provider;
+
+use Drupal\video\ProviderPluginBase;
+
+/**
+ * @VideoEmbeddableProvider(
+ *   id = "youtube",
+ *   label = @Translation("YouTube"),
+ *   description = @Translation("YouTube Video Provider"),
+ *   regular_expressions = {
+ *     "@(?:(?<protocol>http|https):)?//(?:www\.)?youtube(?<cookie>-nocookie)?\.com/embed/(?<id>[a-z0-9_-]+)@i",
+ *     "@(?:(?<protocol>http|https):)?//(?:www\.)?youtube(?<cookie>-nocookie)?\.com/v/(?<id>[a-z0-9_-]+)@i",
+ *     "@(?:(?<protocol>http|https):)?//(?:www\.)?youtube(?<cookie>-nocookie)?\.com/watch(\?|\?.*\&)v=(?<id>[a-z0-9_-]+)@i",
+ *     "@(?:(?<protocol>http|https):)?//youtu(?<cookie>-nocookie)?\.be/(?<id>[a-z0-9_-]+)@i"
+ *   },
+ *   mimetype = "video/youtube",
+ *   stream_wrapper = "youtube"
+ * )
+ */
+class YouTube extends ProviderPluginBase {
+  
+  /**
+   * {@inheritdoc}
+   */
+  public function renderEmbedCode($settings) {
+    $file = $this->getVideoFile();
+    $data = $this->getVideoMetadata();
+    return [
+      '#type' => 'html_tag',
+      '#tag' => 'iframe',
+      '#attributes' => [
+        'width' => $settings['width'],
+        'height' => $settings['height'],
+        'frameborder' => '0',
+        'allowfullscreen' => 'allowfullscreen',
+        'src' => sprintf('https://www.youtube.com/embed/%s?autoplay=%d&start=%d', $data['id'], $settings['autoplay'], NULL),
+      ],
+    ];
+  }
+  
+  /**
+   * {@inheritdoc}
+   */
+  public function getRemoteThumbnailUrl() {
+    $data = $this->getVideoMetadata();
+    return 'http://img.youtube.com/vi/' . $data['id'] . "/maxresdefault.jpg";
+  }
+}
